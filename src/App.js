738383ec45608ccd9f6da27f14463f1b7dba95a6
Maylon-tech <minatokiroisen77@gmail.com>
2022-10-01 15:31:56 +0900

    Configurando layout e estruturas do App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from "@emotion/react"
+import { BrowserRouter } from "react-router-dom"
+
+import { AppRoutes } from "./routes"
+import { LightTheme } from "./shared/themes"
+
+
+function App() {
+
+
+	return (
+		<ThemeProvider theme={LightTheme}>
+			<BrowserRouter>
+
+				<AppRoutes />
+					
+			</BrowserRouter>
+		</ThemeProvider>
+	)
+}
+
+export default App
